f5ea2ddca99d60f677026b1a1bd583d28e0180c6
emsesc <emseschen@gmail.com>
2022-04-17 18:25:05 -0400

    template image + html

--- dynamic-cat-api/index.js
+++ dynamic-cat-api/index.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
-
+const fs = require('fs');
+const path = require('path');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -14,19 +15,21 @@ module.exports = async function (context, req) {
             'x-api-key': process.env.CAT_KEY
         }
     });
-    const url = await urlResp.data[0];
+    const url = await urlResp.data[0].url;
 
-    // get random cat image
-    const imageResp = await fetch(url, {
-        method: 'GET'
-    });
-    const data = await imageResp.arrayBuffer();
-
-    const base64Image = Buffer.from(data).toString('base64');
-
+    // templating html
+    const templateHtml = fs.readFileSync(path.join(__dirname, 'templates.html'), 'utf8');
+    const image = await nodeHtmlToImage({
+        html: templateHtml,
+        content: {
+            text: text,
+            catUrl: url
+        }
+      });
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        header: { "content-type": "image/png" },
+        body: image
     };
 }
