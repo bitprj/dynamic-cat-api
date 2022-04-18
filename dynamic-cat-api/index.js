ea0e9546e4362da21754fef25a161ad43279f683
emsesc <emseschen@gmail.com>
2022-04-17 20:46:18 -0400

    works locally

--- dynamic-cat-api/index.js
+++ dynamic-cat-api/index.js
@@ -1,5 +1,6 @@
 const nodeHtmlToImage = require('node-html-to-image');
-const cats = require("cats-js")
+// const SomeRandomCat = require('some-random-cat').Random;
+const fetch = require('node-fetch')
 const fs = require('fs');
 const path = require('path');
 
@@ -10,10 +11,16 @@ module.exports = async function (context, req) {
     var text = context.bindingData.text;
 
     // get random cat image url
-    const c = new cats();
-    const catObj = await c.get()
-    const url = catObj.images.image.url;
-    context.log(url)
+    // const catObj = await SomeRandomCat.getCat()
+    // const url = catObj.url;
+    const urlResp = await fetch("https://api.thecatapi.com/v1/images/search?format=json", {
+        method: 'GET',
+        headers: {
+            'x-api-key': process.env.CAT_KEY
+        }
+    });
+    const catObj = await urlResp.json();
+    const url = catObj[0].url;
 
     // templating html
     const templateHtml = fs.readFileSync(path.join(__dirname, 'templates.html'), 'utf8');
