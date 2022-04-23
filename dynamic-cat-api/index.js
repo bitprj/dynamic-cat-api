8907f9c90f3dbb434ce38cea5c89da88ab0303ea
emsesc <emseschen@gmail.com>
2022-04-22 20:46:15 -0400

    add color parameter

--- dynamic-cat-api/index.js
+++ dynamic-cat-api/index.js
@@ -6,23 +6,48 @@ module.exports = async function (context, req) {
 
     // get text from route parameter
     var text = context.bindingData.text;
+    var color = req.query.color;
+    var hexcode = "";
+    var size = req.query.size;
+
+    // determine xor value
+    if (color == "green") {
+        hexcode = '#00ff00';
+    } else if (color == "red") {
+        hexcode = '#ff0000';
+    } else if (color == "blue") {
+        hexcode = '#0000ff'
+    } else {
+        hexcode = 'ffffff'
+    }
 
     const buffer = await getImageFromStorage();
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
  
     const image = await Jimp.read(buffer).then(img => {
         return img.scaleToFit(600,600)
     }).then(img=>{
-        return img
-        .print(font,0,0, {
-            text: text,
-            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-            alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM
-          },
-          img.bitmap.width,
-          img.bitmap.height)
-        .getBufferAsync(Jimp.MIME_JPEG);  
+        let textImage = new Jimp(1000,1000, 0x0, (err, textImage) => {  
+            //((0x0 = 0 = rgba(0, 0, 0, 0)) = transparent)
+            if (err) throw err;
+        })
+    
+    
+        textImage.print(font, 0, 0, text)
+        textImage.color([{ apply: 'xor', params: [hexcode] }]); 
+        img.blit(textImage, 0, 0);
+        return img.getBufferAsync(Jimp.MIME_JPEG);
     });
+
+        // return img
+        // .print(font,0,0, {
+        //     text: text,
+        //     alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        //     alignmentY: Jimp.VERTICAL_ALIGN_BOTTOM
+        //   },
+        //   img.bitmap.width,
+        //   img.bitmap.height)
+        // .getBufferAsync(Jimp.MIME_JPEG);  
 
     context.res = {
         header: { "content-type": "image/jpg" },
